9ef0e150e145fb0f2cfaa21a373608d3c0668aa5
haquenafeem <haquenafeem@gmail.com>
2023-12-14 15:08:36 +0600

    client_test.go updated;added test for post,postunmarsha

--- client_test.go
+++ client_test.go
@@ -128,3 +128,84 @@ func Test_GetUnmarshal(t *testing.T) {
 		}
 	}
 }
+
+var PostTestCases = []struct {
+	Title string
+	Path  string
+	Data  struct {
+		UserID    int    `json:"userId"`
+		Title     string `json:"title"`
+		Completed bool   `json:"completed"`
+	}
+	ExpectedValues struct {
+		ID int `json:"id"`
+	}
+}{
+	{
+		Title: `Sending Post Request to "https://jsonplaceholder.typicode.com/todos" should return and match expected values`,
+		Path:  "/todos",
+		Data: struct {
+			UserID    int    `json:"userId"`
+			Title     string `json:"title"`
+			Completed bool   `json:"completed"`
+		}{
+			UserID:    1,
+			Title:     "delectus aut autem",
+			Completed: false,
+		},
+		ExpectedValues: struct {
+			ID int `json:"id"`
+		}{
+			ID: 201,
+		},
+	},
+}
+
+func Test_Post(t *testing.T) {
+	client := New(BaseURL)
+	ctx := context.Background()
+	for _, testCase := range PostTestCases {
+		t.Log(testCase.Title)
+
+		resp, err := client.Post(ctx, testCase.Path, &testCase.Data)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var resStruct struct {
+			ID int `json:"id"`
+		}
+
+		err = json.NewDecoder(resp.Body).Decode(&resStruct)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resStruct.ID != testCase.ExpectedValues.ID {
+			t.Fail()
+		}
+
+		resp.Body.Close()
+	}
+}
+
+func Test_PostUnmarshal(t *testing.T) {
+	client := New(BaseURL)
+	ctx := context.Background()
+	for _, testCase := range PostTestCases {
+		t.Log(testCase.Title)
+
+		var resStruct struct {
+			ID int `json:"id"`
+		}
+
+		err := client.PostUnmarshal(ctx, testCase.Path, &testCase.Data, &resStruct)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resStruct.ID != testCase.ExpectedValues.ID {
+			t.Fail()
+		}
+	}
+}
